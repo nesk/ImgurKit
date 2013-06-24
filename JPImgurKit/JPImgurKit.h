ef4ea15369a83f4485a436d42b19142ea585f0d1
Johann Pardanaud <pardanaud.j@gmail.com>
2013-06-24 10:22:38 +0200

    Initial commit

---
+++ JPImgurKit/JPImgurKit.h
@@ -0,0 +1,13 @@
+//
+//  JPImgurKit.h
+//  JPImgurKit
+//
+//  Created by Johann Pardanaud on 24/06/13.
+//  Copyright (c) 2013 Johann PARDANAUD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JPImgurKit : NSObject
+
+@end
---
+++ JPImgurKit/JPImgurKit.m
@@ -0,0 +1,13 @@
+//
+//  JPImgurKit.m
+//  JPImgurKit
+//
+//  Created by Johann Pardanaud on 24/06/13.
+//  Copyright (c) 2013 Johann PARDANAUD. All rights reserved.
+//
+
+#import "JPImgurKit.h"
+
+@implementation JPImgurKit
+
+@end
